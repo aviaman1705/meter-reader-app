9883782f2a4df468ae09e13ca57b62b0b063ef35
Avi Aman <aviaman1705@gmail.com>
2024-02-22 09:04:44 +0200

    Replace react bootstrap navbar to regular html

--- meter-reader-cms/src/Header.tsx
+++ meter-reader-cms/src/Header.tsx
@@ -26,124 +26,116 @@ export default function Header() {
   }, [claims]);
 
   return (
-    <Navbar
-      bg="custom"
-      variant="dark"
-      sticky="top"
-      expand="lg"
-      dir="rtl"
-      collapseOnSelect
-      expanded={expanded}
-    >
-      <Navbar.Brand>
-        <NavLink className="nav-link" to="/" exact={true}>
-          <FcElectricity title="מערכת קריאת מונים" />
-        </NavLink>
-      </Navbar.Brand>
-      <Navbar.Toggle
-        id="close-button"
-        style={{
-          background: `url("../../icons/hamburger-icon.svg")`,
-          backgroundSize: 28,
-          border: "none",
-        }}
-        aria-controls="navbarScroll"
-        onClick={() => setexpanded(expanded ? false : true)}
-      />
-      <Navbar.Collapse id="navbarScroll">
-        <Nav
-          className="ms-auto my-2 my-lg-0 pe-2"
-          style={{ maxHeight: "120px" }}
-          navbarScroll
-        >
-          <NavLink
-            className="nav-link"
-            to="/"
-            exact={true}
-            onClick={() => setexpanded(false)}
-          >
-            עמוד הבית
+    <>
+      <nav className="navbar navbar-expand-sm navbar-dark bg-dark fixed-top">
+        <div className="container-fluid">
+          <NavLink className="navbar-brand" to="/" exact={true}>
+            <FcElectricity title="מערכת קריאת מונים" />
+            מערכת קריאת מונים
           </NavLink>
-          <Authorized
-            role="admin"
-            authorized={
-              <>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li className="nav-item">
                 <NavLink
                   className="nav-link"
-                  to="/tracks"
+                  to="/"
+                  exact={true}
                   onClick={() => setexpanded(false)}
                 >
-                  מסלולים
+                  עמוד הבית
                 </NavLink>
-                <NavLink
-                  className="nav-link"
-                  to="/notebooks"
-                  onClick={() => setexpanded(false)}
-                >
-                  פנקסים
-                </NavLink>
-                <NavLink
-                  className="nav-link"
-                  to="/statistics"
-                  onClick={() => setexpanded(false)}
-                >
-                  סטטיסטיקות
-                </NavLink>
-              </>
-            }
-          />
-        </Nav>
-        <Nav className="d-flex pe-2">
-          <Authorized
-            role="admin"
-            authorized={
-              <>
-                <NavDropdown title={username} id="navbarScrollingDropdown">
-                  <NavDropdown.Item
-                    href="#"
-                    onClick={() => {
-                      setexpanded(false);
-                      logout();
-                      update([]);
-                    }}
-                  >
-                    יציאה
-                  </NavDropdown.Item>
-                </NavDropdown>
-
-                <a
-                  id="logout-mobile-btn"
-                  onClick={() => {
-                    setexpanded(false);
-                    logout();
-                    update([]);
-                  }}
-                >
-                  יציאה
-                </a>
-              </>
-            }
-            notAuthorized={
-              <>
-                <NavLink
-                  className="nav-link"
-                  to="/login"
-                  onClick={() => setexpanded(false)}
-                >
-                  כניסה
-                </NavLink>
-                <NavLink
-                  className="nav-link"
-                  to="/register"
-                  onClick={() => setexpanded(false)}
-                >
-                  הרשמה
-                </NavLink>
-              </>
-            }
-          />
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
+              </li>
+              <Authorized
+                role="admin"
+                authorized={
+                  <>
+                    <li className="nav-item">
+                      <NavLink
+                        className="nav-link"
+                        to="/tracks"
+                        onClick={() => setexpanded(false)}
+                      >
+                        מסלולים
+                      </NavLink>
+                    </li>
+                    <li className="nav-item">
+                      <NavLink
+                        className="nav-link"
+                        to="/notebooks"
+                        onClick={() => setexpanded(false)}
+                      >
+                        פנקסים
+                      </NavLink>
+                    </li>
+                    <li className="nav-item">
+                      <NavLink
+                        className="nav-link"
+                        to="/statistics"
+                        onClick={() => setexpanded(false)}
+                      >
+                        סטטיסטיקות
+                      </NavLink>
+                    </li>
+                  </>
+                }
+              />
+            </ul>
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <Authorized
+                role=""
+                authorized={
+                  <li className="nav-item">
+                    <a
+                      id="logout-mobile-btn"
+                      className="nav-link"
+                      onClick={() => {
+                        setexpanded(false);
+                        logout();
+                        update([]);
+                      }}
+                    >
+                      יציאה
+                    </a>
+                  </li>
+                }
+                notAuthorized={
+                  <>
+                    <li className="nav-item">
+                      <NavLink
+                        className="nav-link"
+                        to="/login"
+                        onClick={() => setexpanded(false)}
+                      >
+                        כניסה
+                      </NavLink>
+                    </li>
+                    <li className="nav-item">
+                      <NavLink
+                        className="nav-link"
+                        to="/register"
+                        onClick={() => setexpanded(false)}
+                      >
+                        הרשמה
+                      </NavLink>
+                    </li>
+                  </>
+                }
+              />
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
   );
 }
